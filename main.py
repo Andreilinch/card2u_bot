5d6dd4a29ea503ad4b01ae499b484487c158b3a7
Andrey Korotkiy <Andreikorotkiy@gmail.com>
2023-01-05 02:08:30 +0300

    response with image first version

--- main.py
+++ main.py
@@ -2,17 +2,18 @@ import os
 import telebot
 from telebot import types
 from dotenv import load_dotenv
+from models.back_generator import image
 
 load_dotenv()
 
 bot = telebot.TeleBot(os.getenv("TOKEN"))
+
 
 @bot.message_handler(commands=["start"])
 def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     item1 = types.KeyboardButton("🌄 Случайный текст 🌠")
     item2 = types.KeyboardButton("Другое ➡️")
-
 
     markup.add(item1, item2)
 
@@ -30,6 +31,8 @@ def start(message):
 @bot.message_handler()
 def interaction(message):
     bot.send_message(message.chat.id, message.text)
+    image()
+    bot.send_photo(message.chat.id, photo=open("./output/image_.png", "rb"))
 
 
 bot.polling(non_stop=True)
